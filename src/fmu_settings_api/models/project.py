856a463115406adf8db60c31e57d3daccbb7969b
Muhammad Gibran Alfarizi <muga@equinor.com>
2026-09-18 16:04:32 +0200

    ENH: Use Sumo API to get list of assets

--- src/fmu_settings_api/models/project.py
+++ src/fmu_settings_api/models/project.py
@@ -92,16 +92,10 @@ class LockStatus(BaseResponseModel):
 
 
 class SumoAsset(BaseResponseModel):
-    """A valid asset in Sumo."""
+    """A Sumo asset available to the user."""
 
     name: str
-    """Name of the asset in Sumo."""
-
-    code: str
-    """Code of the asset in Sumo."""
-
-    roleprefix: str
-    """Roleprefix of the asset in Sumo."""
+    """Name of the asset in Sumo the user has write access to."""
 
 
 class ValidationMismatch(BaseResponseModel):
